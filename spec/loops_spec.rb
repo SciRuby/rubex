de69bf4d133b0f335bcbf8909cc6da9b6d989a94
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2016-12-31 11:04:10 +0530

    Added specs and fixtures for loops. Static arrays need some upgrades.

---
+++ spec/loops_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe Rubex do
+  context "Rubex method static arrays" do
+    before do
+      @path = 'spec/fixtures/loops/loops.rubex'
+    end
+
+    context ".ast" do
+      it "generates the AST" do
+        t = Rubex.ast @path
+      end
+    end
+
+    context ".compile" do
+      it "compiles to valid C file" do
+        # t,c,e = Rubex.compile @path, true
+        # pp t
+        # puts c
+      end
+    end
+  end
+end
