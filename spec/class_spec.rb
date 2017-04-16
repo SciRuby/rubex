c84ae83b34880e2de5c6f1b6fd3f306e2afa11a4
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2017-04-16 14:26:34 +0530

    Made some rectifications and updates to class code sample. Added black box testing to class specs.

--- spec/class_spec.rb
+++ spec/class_spec.rb
@@ -2,19 +2,35 @@ require 'spec_helper'
 
 describe Rubex do
   test_case = 'class'
-  path = "spec/fixtures/#{test_case}/#{test_case}"
 
-  context "File: #{path}.rubex" do
+  context "Case: #{test_case}" do
+    before do
+      @path = path_str test_case
+    end
+
     context ".ast" do
       it "generates the AST" do
-        t = Rubex.ast(path + '.rubex')
-        pp t
+        t = Rubex.ast(@path + '.rubex')
       end
     end
 
     context ".compile" do
       it "compiles to valid C file" do
-        t,c,e = Rubex.compile((path + '.rubex'), true)
+        t,c,e = Rubex.compile(@path + '.rubex', test: true)
+      end
+    end
+
+    context "Black Box testing" do
+      it "compiles and checks for valid output" do
+        setup_and_teardown_compiled_files(test_case) do |dir|
+          require_relative "#{dir}/#{test_case}.so"
+
+          k = Kustom.new
+          expect(k.bye).to eq("Bye world!")
+
+          k2 = Kustom2.new
+          expect(k2.hello).to eq("This is a prelude.Hello world!")
+        end
       end
     end
   end
