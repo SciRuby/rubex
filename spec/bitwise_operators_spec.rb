a94073ba60d31c4416a2635414129a4676adc85d
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2017-05-05 21:47:55 +0530

    Added tests for bitwise operations.

---
+++ spec/bitwise_operators_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+include Rubex::AST
+
+describe Rubex do
+  test_case = 'bitwise_operators'
+
+  context "Case : #{test_case}" do
+    before do
+      @path = path_str test_case
+    end
+
+    context ".ast" do
+      it "returns a valid Abstract Syntax Tree" do
+        t = Rubex.ast @path + ".rubex"
+      end
+    end
+
+    context ".compile", focus: true do
+      it "generates valid C code" do
+        t, c, e = Rubex.compile @path + ".rubex", test: true
+      end
+    end
+
+    context "Black Box testing" do
+      it "compiles and checks for valid output" do
+        setup_and_teardown_compiled_files(test_case) do |dir|
+          require_relative "#{dir}/#{test_case}.so"
+
+          cls = BitWise.new
+          expect(cls.wise_or).to eq(1)
+          expect(cls.wise_and).to eq(1)
+          expect(cls.wise_compli).to eq(1)
+          expect(cls.wise_lshift).to eq(1)
+          expect(cls.wise_rshift).to eq(1)
+        end
+      end
+    end
+  end
+end
