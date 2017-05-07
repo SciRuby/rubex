3a7956dd521841afc611d3d81579811c14135d9d
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2017-05-07 15:32:55 +0530

    Started with tests for calling C functions by reference.

---
+++ spec/call_by_reference_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe Rubex do
+  test_case = 'call_by_reference'
+
+  context "Case: #{test_case}" do
+    before do
+      @path = path_str test_case
+    end
+
+    context ".ast" do
+      it "generates the AST" do
+        t = Rubex.ast(@path + '.rubex')
+      end
+    end
+
+    context ".compile" do
+      it "compiles to valid C file" do
+        t,c,e = Rubex.compile(@path + '.rubex', test: true)
+      end
+    end
+
+    context "Black Box testing" do
+      it "compiles and checks for valid output" do
+        setup_and_teardown_compiled_files(test_case) do |dir|
+          require_relative "#{dir}/#{test_case}.so"
+
+          expect(CallByReference.new.ref_call).to eq(10)
+        end
+      end
+    end
+  end
+end
