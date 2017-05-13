bddf08abe2905d4f543c97e36c173853256f96ef
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2017-05-13 22:41:40 +0530

    added tests for function pointers.

---
+++ spec/c_function_ptrs_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+include Rubex::AST
+
+describe Rubex do
+  test_case = 'c_function_ptrs'
+
+  context "Case : #{test_case}" do
+    before do
+      @path = path_str test_case
+    end
+
+    context ".ast" do
+      it "returns a valid Abstract Syntax Tree" do
+        t = Rubex.ast @path + ".rubex"
+      end
+    end
+
+    context ".compile" do
+      it "generates valid C code" do
+        t, c, e = Rubex.compile @path + ".rubex", test: true
+      end
+    end
+
+    context "Black Box testing" do
+      it "compiles and checks for valid output" do
+        setup_and_teardown_compiled_files(test_case) do |dir|
+          require_relative "#{dir}/#{test_case}.so"
+
+          cls = CFunctionPtrs.new
+
+          expect(cls.test_function_ptrs(true)) .to eq(5)
+          expect(cls.test_function_ptrs(false)).to eq(7)
+        end
+      end
+    end
+  end
+end
