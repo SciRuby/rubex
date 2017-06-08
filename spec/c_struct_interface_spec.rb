f2591d76a55b12ddf047c5803f34b08607ed1c06
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2017-06-08 22:08:58 +0530

    add spec files for error handling an c struct interface

---
+++ spec/c_struct_interface_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+include Rubex::AST
+
+describe Rubex do
+  test_case = 'c_struct_interface'
+
+  context "Case : #{test_case}" do
+    before do
+      @path = path_str test_case
+    end
+
+    context ".ast" do
+      it "returns a valid Abstract Syntax Tree" do
+        t = Rubex.ast @path + ".rubex"
+      end
+    end
+
+    context ".compile" do
+      it "generates valid C code" do
+        t, c, e = Rubex.compile @path + ".rubex", test: true
+      end
+    end
+
+    context "Black Box testing" do
+      it "compiles and checks for valid output" do
+        setup_and_teardown_compiled_files(test_case) do
+          dir = dir_str test_case
+          require_relative "#{dir}/#{test_case}.so"
+          
+          m = Music.new("Animals as Leaders", "CAFO", 1)
+
+          expect(m.artist).to eq("Animals as Leaders")
+          expect(m.title) .to eq("CAFO")
+          expect(m.id)    .to eq(1)
+        end
+      end
+    end
+  end
+end
---
+++ spec/error_handling_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe Rubex do
+  test_case = "error_handling"
+
+  context "Case: #{test_case}" do
+    before do
+      @path = path_str test_case
+    end
+
+    context ".ast" do
+      it "generates the AST" do
+        t = Rubex.ast(@path + '.rubex')
+      end
+    end
+
+    context ".compile" do
+      it "compiles to valid C file" do
+        t,c,e = Rubex.compile(@path + '.rubex', test: true)
+      end
+    end
+
+    context "Black box testing" do
+      it "compiles and checks for valid output" do
+        setup_and_teardown_compiled_files(test_case) do |dir|
+          require_relative "#{dir}/#{test_case}.so"
+
+          cls = Handler.new
+
+          expect(cls.error_test(1)).to eq(6)
+          expect(cls.error_test(2)).to eq(7)
+          expect(cls.error_test(3)).to eq(8)
+          expect(cls.error_test(4)).to eq(9)
+        end
+      end
+    end
+  end
+end
