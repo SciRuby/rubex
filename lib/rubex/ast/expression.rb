c09227a6f4e121cb4753d16af3826c38a65ad6af
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2017-07-24 22:18:09 +0530

    Wrote functionality for hash literal assignment.

--- lib/rubex/ast/expression.rb
+++ lib/rubex/ast/expression.rb
@@ -33,6 +33,18 @@ module Rubex
         def from_ruby_object
           FromRubyObject.new self
         end
+
+        def generate_evaluation_code code, local_scope
+          c_code(local_scope)
+        end
+
+        def generate_disposal_code code
+
+        end
+
+        def generate_assignment_code rhs, code, local_scope
+          
+        end
       end
 
       class Typecast < Base
@@ -316,7 +328,7 @@ module Rubex
         end
 
         def generate_assignment_code rhs, code, local_scope
-          code << "#{self.c_code(local_scope)} = #{rhs.c_code(local_scope)}"
+          code << "#{self.c_code(local_scope)} = #{rhs.c_code(local_scope)};"
           code.nl
           rhs.generate_disposal_code code
         end
@@ -536,6 +548,18 @@ module Rubex
         def c_code local_scope
           "#{@expr.type.to_ruby_object(@expr.c_code(local_scope))}"
         end
+
+        def generate_evaluation_code code, local_scope
+          @expr.generate_evaluation_code code, local_scope
+        end
+
+        def generate_disposal_code code
+          @expr.generate_disposal_code code
+        end
+
+        def generate_assignment_code rhs, code, local_scope
+          @expr.generate_assignment_code rhs, code, local_scope
+        end
       end
 
       # internal node for converting from ruby object.
@@ -616,8 +640,47 @@ module Rubex
         end
 
         class HashLit < Literal::Base
-          def initialize data_hash
-            @data_hash = data_hash
+          def initialize key_val_pairs
+            @key_val_pairs = key_val_pairs
+          end
+
+          def analyse_statement local_scope
+            @type = Rubex::DataType::RubyObject.new
+            @c_code = local_scope.allocate_temp @type
+            @key_val_pairs.map! do |k, v|
+              k.analyse_for_target_type(@type, local_scope)
+              v.analyse_for_target_type(@type, local_scope)
+              [k.to_ruby_object, v.to_ruby_object]
+            end
+
+            ap @key_val_pairs
+            local_scope.release_temp @c_code
+          end
+
+          def generate_evaluation_code code, local_scope
+            code << "#{@c_code} = rb_hash_new();"
+            code.nl
+            @key_val_pairs.each do |k, v|
+              k.generate_evaluation_code(code, local_scope)
+              v.generate_evaluation_code(code, local_scope)
+
+              code << "rb_hash_new_element(#{@c_code}, #{k.c_code(local_scope)}, "
+              code << "#{v.c_code(local_scope)});"
+              code.nl
+
+              k.generate_disposal_code code
+              v.generate_disposal_code code
+              code.nl
+            end            
+          end
+
+          def generate_disposal_code code
+            code << "#{@c_code} = 0"
+            code.nl
+          end
+
+          def c_code local_scope
+            @c_code
           end
         end
 
