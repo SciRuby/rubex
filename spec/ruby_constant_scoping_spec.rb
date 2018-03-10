54914a690c7d1c4a44d3fcaf5cc3352193d576f6
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2018-03-11 01:36:57 +0900

    wrote tests and added fixtures for constant scoping and outside stmts

---
+++ spec/ruby_constant_scoping_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+include Rubex::AST
+
+describe Rubex do
+  test_case = 'ruby_constant_scoping'
+
+  context "Case : #{test_case}" do
+    before do
+      @path = path_str test_case
+    end
+
+    context ".ast" do
+      it "returns a valid Abstract Syntax Tree" do
+        t = Rubex::Compiler.ast @path + ".rubex"
+      end
+    end
+
+    context ".compile" do
+      it "generates valid C code" do
+        t, c, e = Rubex::Compiler.compile @path + ".rubex", test: true
+      end
+    end
+
+    context "Black Box testing" do
+      it "compiles and checks for valid output" do
+        setup_and_teardown_compiled_files(test_case) do |dir|
+          require_relative "#{dir}/#{test_case}.#{os_extension}"
+          
+          expect(get_eid.is_a?(Fixnum)).to eq(true)
+        end
+      end
+    end
+  end
+end
