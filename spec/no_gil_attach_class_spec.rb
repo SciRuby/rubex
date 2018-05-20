e0742a9530878478988a4e6f1f1cf48240fb3bc2
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2018-05-20 16:32:18 +0900

    added new tests for edge case with attached class and no_gil. skip for now.

---
+++ spec/no_gil_attach_class_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe Rubex do
+  test_case = "no_gil_attach_class"
+
+  context "Case: #{test_case}" do
+    before do
+      @path = path_str test_case
+    end
+
+    context ".ast" do
+      it "generates the AST" do
+        t = Rubex::Compiler.ast(@path + '.rubex')
+      end
+    end
+
+    context ".compile" do
+      skip "compiles to valid C file" do
+        t,c,e = Rubex::Compiler.compile(@path + '.rubex', test: true)
+      end
+    end
+
+    context "Black Box testing" do
+      skip "compiles and checks for valid output" do
+        setup_and_teardown_compiled_files(test_case) do |dir|
+          require_relative "#{dir}/#{test_case}.#{os_extension}"
+
+          expect(A.new.foo)
+        end
+      end
+    end
+  end
+end
