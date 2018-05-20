6743ec7dd2bc4b5423d2eb70fc8b91ec658ae90e
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2018-05-20 15:02:49 +0900

    this

--- lib/rubex/ast/top_statement/klass/attached_klass.rb
+++ lib/rubex/ast/top_statement/klass/attached_klass.rb
@@ -132,7 +132,7 @@ module Rubex
         def member_struct_allocations
           c_name = @scope.find(@attached_type).c_name
           binding.pry
-          data->#{Rubex::POINTER_PREFIX + @attached_type} = (#{c_name}*)xmalloc(sizeof(#{c_name}));\n"
+         "data->#{Rubex::POINTER_PREFIX + @attached_type} = (#{c_name}*)xmalloc(sizeof(#{c_name}));\n"
         end
 
         # Actually write the dealloc function into C code.
